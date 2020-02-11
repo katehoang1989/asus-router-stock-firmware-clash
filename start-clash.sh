756b2f3358b163cee6996d3bf0c9bae771df7e8b
katehoang1989 <60130336+katehoang1989@users.noreply.github.com>
2020-02-11 14:20:37 +0700

    Update start-clash.sh

--- start-clash.sh
+++ start-clash.sh
@@ -8,13 +8,13 @@ create_iptables_rules() {
     /usr/sbin/iptables -t nat -A CLASH -d 192.168.0.0/16 -j RETURN
     /usr/sbin/iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 9090
     /usr/sbin/iptables -t nat -A PREROUTING -j CLASH
-    /usr/sbin/iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j DNAT --to-destination 192.168.50.1:12358
+    /usr/sbin/iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j DNAT --to-destination 192.168.11.1:12358
   fi
 }
 
 start_clash() {
   killall clash-linux-armv8
-  nohup /mnt/sda1/clash-linux-armv8 -d /mnt/sda1/ > /mnt/sda1/nohup.out &
+  nohup /mnt/ASUS/clash-linux-armv8 -d /mnt/ASUS/ > /mnt/ASUS/nohup.out &
 }
 
 create_iptables_rules
